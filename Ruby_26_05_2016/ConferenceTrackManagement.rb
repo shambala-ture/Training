77bac686752f09036d45ff745252258e21a5513d
shambala.ture <shambala.ture@wwindia.com>
2016-05-27 20:30:03 +0530

    Conference Track Management assignment

--- Ruby_26_05_2016/ConferenceTrackManagement.rb
+++ Ruby_26_05_2016/ConferenceTrackManagement.rb
@@ -31,19 +31,48 @@ doc.each do|i|
    task_time_list.push(time)
   end
 end
+ #puts task_time_list.inspect
+ #puts task_list.inspect
+
 start_time = Time.new(2016, 10, 31, 9, 0, 0)
-
+current_time = Time.new(2016, 10, 31, 9, 0, 0)
 task_time_list.each_with_index {|val, index|
-  # puts "#{val}  #{task_list[index]}"
-  puts "#{start_time.strftime("%H:%M %p")}  #{task_list[index]}"
-  start_time= start_time + val.to_i * 60
+
+  if (start_time.strftime("%I:%M %p") == "12:00 PM")
+    puts "#{start_time.strftime("%I:%M %p")}  Lunch"
+    start_time= start_time + 60*60
+    redo
+  elsif (start_time.hour == 16)
+    puts "#{start_time.strftime("%I:%M %p")}  Networking Event"
+     start_time= start_time + 60*60 
+     puts "============End for day=========="
+     puts "============Track 2=============="
+     start_time = Time.new(2016, 10, 31, 9, 0, 0)
+     redo
+  else
+    puts "#{start_time.strftime("%I:%M %p")}  #{task_list[index]}"
+    start_time= start_time + val.to_i * 60
+  end
+
+
 }
+puts "End list"
+
+
+  # puts "#{val}  #{task_list[index]}"
+  # puts "#{start_time.strftime("%I:%M %p")}  #{task_list[index]}"
+  # start_time= start_time + val.to_i * 60
 
 
 
 
-
-
+  # if (val == 12)
+  #   task_list.push("Lunch 60min")
+  #   elsif (val == 5)
+  #   task_list.push("Networking Event")
+  #   else
+  #     puts ("End task for today")
+  # end
 # puts start_time
 # puts task_list
 # puts task_time_list
