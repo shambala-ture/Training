929c16539f54541d212edf46e2f06fb092aa9552
shambala.ture <shambala.ture@wwindia.com>
2016-06-13 17:14:33 +0530

    Assignment 2 in rails

---
+++ work.rb
@@ -0,0 +1,7 @@
+Title:        A Simple Twitter App with Ruby on Rails – Messages With Ajax (18 Hours)
+Description:  Basic user authentication
+              Upload profile images
+              Social site integration
+              Allow the user to post tweet in a similar way to Twitter
+              Allow users and followers to re-tweet / comment on tweets
+              Add concept of following just like twitter.
